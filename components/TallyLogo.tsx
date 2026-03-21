2e4c30cdc5b7efb2619f2e93bde0be20dcec5eaf
jpanepinto23 <jpanepinto23@gmail.com>
2026-03-21 18:15:54 -0400

    Create TallyLogo.tsx

---
+++ components/TallyLogo.tsx
@@ -0,0 +1,13 @@
+export default function TallyLogo({ size = 1 }: { size?: number }) {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', gap: 12 * size, justifyContent: 'center' }}>
+      <div style={{ position: 'relative', width: 44 * size, height: 36 * size }}>
+        {[6, 14, 22, 30].map((left, i) => (
+          <div key={i} style={{ position: 'absolute', left: left * size, top: 4 * size, width: 4 * size, height: 28 * size, background: '#111110', borderRadius: 2 }} />
+        ))}
+        <div style={{ position: 'absolute', top: 16 * size, left: -2 * size, width: 48 * size, height: 3.5 * size, background: '#B5593C', borderRadius: 2, transform: 'rotate(-30deg)' }} />
+      </div>
+      <span style={{ fontFamily: 'Archivo, sans-serif', fontSize: 20 * size, fontWeight: 900, letterSpacing: 6 * size, textTransform: 'uppercase', color: '#111110' }}>COUNT</span>
+    </div>
+  )
+}
